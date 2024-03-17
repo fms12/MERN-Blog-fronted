6163ffa91895025e3852255c4cd768a88a00afa8
fms12 <mayank2001.iimt@gmail.com>
2024-03-18 03:01:19 +0530

    changes

--- src/features/auth/components/Login.js
+++ src/features/auth/components/Login.js
@@ -91,12 +91,12 @@ export default function Login() {
                   type="password"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
-                {errors.password && (
-                  <p className="text-red-500">{errors.password.message}</p>
+                {errors.message&& (
+                  <p className="text-red-500">{errors.message}</p>
                 )}
               </div>
               {error && (
-                <p className="text-red-500">{error || error.message}</p>
+                <p className="text-red-500">{error.message}</p>
               )}
             </div>
 
